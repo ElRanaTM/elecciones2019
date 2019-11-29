4c205339f904860bda1f3c934d50e6fcca0d93b0
David <40724454+ElRanaTM@users.noreply.github.com>
2019-11-29 12:59:53 -0400

    ahora imprime los votos en gral

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 from time import time
 
-# import numpy as np
+import regresion_polinomial
+
+import numpy as np
 import pandas as pd
-# import matplotlib
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+# from sklearn import linear_model
+# from sklearn.model_selection import train_test_split
 
 tiempo_inicial = time()
 actas = pd.read_excel('actas.xlsx')
@@ -37,6 +39,53 @@ y = actas[['Votos Válidos']]
 print(y.info())
 input("Presione ENTER para continuar...")
 
+newColumn = np.power(X, 7)
+print(newColumn.shape)
+
+X_x2agregado = np.append(X, newColumn, 1)
+print(X_x2agregado.shape)
+
+plt.plot(X_x2agregado[:, 0], y, "y.", ms=1)
+plt.title("Comunidad Ciudadana")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+plt.plot(X_x2agregado[:, 1], y, "m.", ms=1)
+plt.title("Frente Para la Victoria")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+plt.plot(X_x2agregado[:, 2], y, "g.", ms=1)
+plt.title("Movimiento Tercer Sistema")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+plt.plot(X_x2agregado[:, 3], y, "b.", ms=1)
+plt.title("Movimiento Al Socialismo")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+plt.plot(X_x2agregado[:, 4], y, "r.", ms=1)
+plt.title("Bolivia Dice No 21F")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+plt.plot(X_x2agregado[:, 5], y, "k.", ms=1)
+plt.title("Partido Democrata Cristiano")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+plt.plot(X_x2agregado[:, 6], y, "c.", ms=1)
+plt.title("Movimiento Nacionalista Revolucionario")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+plt.plot(X_x2agregado[:, 7], y, "r.", ms=1)
+plt.title("Partido de Accion Nacional Boliviano")
+plt.xlabel('Cabtidad de Votantes')
+plt.ylabel('Cantidad de inscritos')
+plt.show()
+'''
 # Entrenamos los datos
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
@@ -51,11 +100,12 @@ Y_pred_multiple = lr_multiple.predict(X_test)
 
 print('DATOS DEL MODELO REGRESIÓN LINEAL MULTIPLE')
 print()
-print('Valor de las pendientes:')
+print('Valor de las pendientes o coeficientes "a":')
 print(lr_multiple.coef_)
-print('Valor de la intersección:')
+print('Valor de la intersección o coeficiente "b":')
 print(lr_multiple.intercept_)
 input("Presione ENTER para continuar...")
 
 print('Precisión del modelo:')
 print(lr_multiple.score(X_train, y_train))
+'''
